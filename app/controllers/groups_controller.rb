0c1a9f29ba141997e06ca783553fd0f0ce9468e4
wellsonjain <wellsonjain@gmail.com>
2015-08-25 18:08:20 +0800

    Add feature user join and quit group
    Signed-off-by: wellsonjain <wellsonjain@gmail.com>

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -32,6 +32,7 @@ class GroupsController < ApplicationController
     @group = current_user.groups.create(group_params)
 
     if @group.save
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
@@ -44,6 +45,31 @@ class GroupsController < ApplicationController
     redirect_to groups_path, alert: "討論版已刪除"
   end
 
+  def join
+    @group = Group.find(params[:id])
+
+    if not current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "Welcome to this board."
+    else
+      flash[:warning] = "You are already the member."
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "You have already quit this board."
+    else
+      flash[:warning] = "You are not the member of this board."
+    end
+
+    redirect_to group_path(@group)
+  end
   private
 
   def group_params
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,8 @@ class PostsController < ApplicationController
 
   before_action :authenticate_user!
   before_action :find_group
+  before_action :member_required, only: [:new, :create]
+
   def new
     @post = @group.posts.new
   end
@@ -46,5 +48,11 @@ class PostsController < ApplicationController
 
   def find_group
     @group = Group.find(params[:group_id])
+  end
+
+  def member_required
+    return if !current_user.is_member_of(@group)
+    flash[:warning] = "You are not the memeber of this private group. Can not post!"
+    redirect_to group_path(@group)
   end
 end
