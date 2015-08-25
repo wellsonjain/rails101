df59bf7900a9c7880259c007bb62779fea9b4f55
wellsonjain <wellsonjain@gmail.com>
2015-08-25 12:57:50 +0800

    Use before action to refactor code
    Signed-off-by: wellsonjain <wellsonjain@gmail.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,15 @@
 class PostsController < ApplicationController
 
+  before_action :find_group
   def new
-    @group = Group.find(params[:group_id])
     @post = @group.posts.new
   end
 
   def edit
-    @group = Group.find(params[:group_id])
     @post = @group.posts.find(params[:id])
   end
 
   def create
-    @group = Group.find(params[:group_id])
     @post = @group.posts.build(post_params)
 
     if @post.save
@@ -22,18 +20,16 @@ class PostsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:group_id])
     @post = @group.posts.find(params[:id])
 
     if @post.update(post_params)
-      redirect_to group_path(@post), notice: "文章修改成功!"
+      redirect_to group_path(@group), notice: "文章修改成功!"
     else
       render :edit
     end
   end
 
   def destroy
-    @group = Group.find(params[:group_id])
     @post = @group.posts.find(params[:id])
 
     @post.destroy
@@ -44,5 +40,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def find_group
+    @group = Group.find(params[:group_id])
   end
 end
