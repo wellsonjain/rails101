36a959fa6dac108004abf44982bfef3e810124d8
wellsonjain <wellsonjain@gmail.com>
2015-08-27 18:28:21 +0800

    Set seed and rake
    Signed-off-by: wellsonjain <wellsonjain@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+create_account = User.create([email: 'example@gmail.com', password: '12345678', password_confirmation: '12345678', name: 'Test Account'])
+
+create_group = for i in 1..20 do
+        Group.create!([title: "Group no.#{i}", description: "Board #{i} be seed", user_id: 3])
+        for k in 1..30 do
+          Post.create!([group_id: "#{i}", content: "Message #{k} by seed.", user_id: 3])
+        end
+      end
